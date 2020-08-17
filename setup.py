16a5bd0cc5dfad4c66d05e2a83e4c6cfb6c2ebd2
Bhavesh Agrawal <bhavesh.agrawal242@gmail.com>
2020-08-16 21:46:54 -0400

    setup.py added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name='group_15',
+        version='v1.0.0',
+        description='Homework_1 : Making a Good Repository',
+        author='Aadil Anwar Khan',
+        author_email='akhan23@ncsu.edu',
+        license='MIT',
+        packages=setuptools.find_packages(),
+        python_requires='>=3.6',
+    )
